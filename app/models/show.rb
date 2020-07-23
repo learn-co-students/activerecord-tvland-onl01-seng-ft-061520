7179ef57830eee4924db8fab81a38299a6bae33d
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-07-22 21:01:58 -0600

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
-end
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actor_list
+      "#{first_name} #{last_name}"
+    end
+  end
